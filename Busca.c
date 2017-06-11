ba1f487854481dbea96b285fdebc1c8ad3121d19
Schatzmann <annelyse94@gmail.com>
2017-06-11 01:06:03 -0300

    então
    ta compilando e até roda se vc por as entradas do site em um arquivo e ler,porém,imprime meio errado :/
    ps:acho que a busca tá errada também
    ps2: falta ainda o remove
    ps3: desculpa meu desespero, quando o trablho de oficina nao da certo venho aqui 
    :*

---
+++ Busca.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvoreAVL.h"
+
+
+
+
+int main(int argc, char **argv){ 
+ FILE *arq;
+ tNo *no = NULL;
+ int valor;
+ char ch;
+ 
+
+	arq = fopen (argv[1], "r") ;
+ 
+	if ( ! arq ){
+    	arq = stdin; 											
+   	}
+
+	fscanf(arq,"%s %d",&ch,&valor); 
+	while(!feof (arq)){
+		switch(ch){
+			case 'i':                     
+				insere(no,valor);
+				printf("i %d\n(",valor);
+				imprime_tree(no);
+				printf(")\n\n");
+			break;
+
+			case 'b':					    
+				printf("b %d\n",valor);
+				busca(no,valor);
+			break;
+
+			case 'r':					 
+				//remove(no,valor);
+				printf("r %d\n(",valor);
+				imprime_tree(no);
+				printf(")\n\n");
+			break;
+		}
+	}
+ fclose(arq);
+ return(0);
+}
