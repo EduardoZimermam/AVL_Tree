6b5f8f89515aa265919b1e05164a7aa309854a90
Schatzmann <annelyse94@gmail.com>
2017-06-14 18:00:35 -0300

    Create Busca.c

--- Busca.c
+++ Busca.c
@@ -31,7 +31,7 @@ int main(int argc, char **argv){
 			break;
 
 			case 'r':					 
-				no = remove(no,valor);
+				no = remocao(no,valor);
 				printf("r %d\n(",valor);
 				imprime_tree(no);
 				printf(")\n\n");
